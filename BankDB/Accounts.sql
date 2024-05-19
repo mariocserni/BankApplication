e95a2b8b41066c4194c52b2fe369f22d07f1d289
Mario Cserni <r_marioroberto@yahoo.com>
2024-05-19 21:20:18 +0300

    Added tables to the database

---
+++ BankDB/Accounts.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[Accounts](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Username] [varchar](20) NOT NULL,
+	[FirstName] [varchar](20) NOT NULL,
+	[LastName] [varchar](20) NOT NULL,
+	[Password] [varchar](50) NOT NULL,
+	[Email] [varchar](50) NOT NULL,
+	[Age] [int] NOT NULL,
+	[Money] [decimal](18, 0) NOT NULL,
+ CONSTRAINT [PK_Accounts] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
